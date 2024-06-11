191ca2bb9ac2acb89d5b3103b523a0cf03d068c8
suresh867 <sureshpathak867@gmail.com>
2024-06-11 10:54:03 +0545

    thisis helloworl

--- app/(sales)/_components/footer/Footer.tsx
+++ app/(sales)/_components/footer/Footer.tsx
@@ -1,16 +1,18 @@
+import { Copyright } from 'lucide-react'
 import React from 'react'
 
 const Footer = () => {
   return (
    <>
-    <footer className='flex justify-around bg-[#31393C] p-2'>
+    <footer className='flex flex-col bg-[#31393C] p-2'>
+        <div className='flex justify-around '>
         <div className='flex flex-col gap-2 w-[25%] p-5 text-white'>
             <h2 className='font-semibold text-xl '>Thesispro Nepal</h2>
             <p className='text-sm'>We offer best assignment help around the world ensuring privacy and plagiarism-free work.</p>
             <h4>Kathmandu, Nepal</h4>
         </div>
         <div className='h-auto w-[1px] bg-white'/>
-        <div className='w-[35%] block p-1'>
+        <div className='w-[35%] block p-5'>
             <h2 className='text-center font-bold text-xl text-white'>Categories</h2>
             <ul className='flex flex-wrap gap-10 p-3 justify-center items-center text-sm text-white font-semibold'>
                 <li className='w-28 h-auto  text-center'>Python</li>
@@ -25,7 +27,7 @@ const Footer = () => {
             </ul>
         </div>
         <div className='h-auto w-[1px] bg-white'/>
-        <div className='flex flex-col gap-2 '>
+        <div className='flex flex-col gap-2 p-5'>
             <h3 className='text-xl text-white font-semibold'>Contact us</h3>
             <ul className='leading-loose text-white text-sm'>
                 <li className='flex gap-2'>
@@ -47,6 +49,10 @@ const Footer = () => {
                     <h5>thesispronepal@gmail.com</h5>
                 </li>
             </ul>
+        </div>
+        </div>
+        <div className='flex items-center justify-center text-white text-sm text-muted-foreground'>
+            <p className='flex items-center'>Copyright <Copyright className='h-4'/> 2024. ThesisProNepal. All Rights Reserved.</p>
         </div>
     </footer>
    </>
