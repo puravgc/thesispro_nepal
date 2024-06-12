8f42f758a876f1a6011252ad1fa6ee26fed04a13
puravgc <puravgc02@gmail.com>
2024-06-12 23:58:01 +0545

    authentication

--- app/(sales)/_components/Navbar/Navbar.tsx
+++ app/(sales)/_components/Navbar/Navbar.tsx
@@ -1,37 +1,44 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const liComponents = [
-    {
-        id: 1, 
-        name: "Home", 
-    }, {
-        id: 2, 
-        name: "Services", 
-    }, 
-    {
-        id: 3, 
-        name: "Contact Us"
-    }
-]
+  {
+    id: 1,
+    name: "Home",
+  },
+  {
+    id: 2,
+    name: "Services",
+  },
+  {
+    id: 3,
+    name: "Contact Us",
+  },
+];
 
 const Navbar = () => {
   return (
     <nav className="sticky top-0 bg-[#C2DDE8] z-10">
       <div className="flex justify-between mx-10 p-3 items-center">
-       <h3>LOGO</h3>
+        <h3>LOGO</h3>
         <ul className="flex justify-around w-[40%]">
-         {
-            liComponents.map((li) => (
-               <li key={li.id} className="hover:text-sky-500 cursor-pointer font-semibold">
-                    {li.name}
-               </li>
-            ))
-         }
+          {liComponents.map((li) => (
+            <li
+              key={li.id}
+              className="hover:text-sky-500 cursor-pointer font-semibold"
+            >
+              {li.name}
+            </li>
+          ))}
         </ul>
         <div className="flex gap-4 ">
-          <Button variant="secondary">Sign Up</Button>
-          <Button variant="secondary">Sign In</Button>
+          <Link href={"/signup"}>
+            <Button variant="secondary">Sign Up</Button>
+          </Link>
+          <Link href={"/signin"}>
+            <Button variant="secondary">Sign In</Button>
+          </Link>
         </div>
       </div>
     </nav>
---
+++ models/userModel.ts
@@ -0,0 +1,30 @@
+import { verify } from "crypto";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: String,
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
