c40e6b1c716871f4bc2fcd7763f2f1b495c3b897
suresh867 <sureshpathak867@gmail.com>
2024-06-12 10:43:23 +0545

    minorfixes

--- app/(sales)/_components/categories/Categories.tsx
+++ app/(sales)/_components/categories/Categories.tsx
@@ -7,37 +7,37 @@ const Categories = () => {
     <div>
       <h2 className="text-center font-semibold text-2xl my-4 relative z-20">Categories</h2>
       <div className="flex justify-between mx-10 my-10">
-        <Card className="bg-[#706565] cursor-pointer hover:scale-150 transition duration-300 ease-in-out">
+        <Card className="bg-[#706565] cursor-pointer hover:scale-150 hover:text-white hover:bg-sky-400 hover:font-semibold transition duration-300 ease-in-out">
           <CardContent className="flex flex-col gap-2 p-2 w-32 h-32 items-center justify-center">
             <FaPython className="w-10 h-10"/>
             <h3>Python</h3>
           </CardContent>
         </Card>
-        <Card className="bg-[#706565] cursor-pointer hover:scale-150 transition duration-300 ease-in-out">
+        <Card className="bg-[#706565] cursor-pointer hover:scale-150 transition duration-300 ease-in-out  hover:text-white hover:bg-sky-400 hover:font-semibold">
           <CardContent className="flex flex-col gap-2 p-2 w-32 h-32 items-center justify-center">
             <FaPython className="w-10 h-10"/>
             <h3>Python</h3>
           </CardContent>
         </Card>
-        <Card className="bg-[#706565] cursor-pointer hover:scale-150 transition duration-300 ease-in-out">
+        <Card className="bg-[#706565] cursor-pointer hover:scale-150 transition duration-300 ease-in-out  hover:text-white hover:bg-sky-400 hover:font-semibold">
           <CardContent className="flex flex-col gap-2 p-2 w-32 h-32 items-center justify-center">
             <FaPython className="w-10 h-10"/>
             <h3>Python</h3>
           </CardContent>
         </Card>
-        <Card className="bg-[#706565] cursor-pointer hover:scale-150 transition duration-300 ease-in-out">
+        <Card className="bg-[#706565] cursor-pointer hover:scale-150 transition duration-300 ease-in-out  hover:text-white hover:bg-sky-400 hover:font-semibold">
           <CardContent className="flex flex-col gap-2 p-2 w-32 h-32 items-center justify-center">
             <FaPython className="w-10 h-10"/>
             <h3>Python</h3>
           </CardContent>
         </Card>
-        <Card className="bg-[#706565] cursor-pointer hover:scale-150 transition duration-300 ease-in-out">
+        <Card className="bg-[#706565] cursor-pointer hover:scale-150 transition duration-300 ease-in-out  hover:text-white hover:bg-sky-400 hover:font-semibold">
           <CardContent className="flex flex-col gap-2 p-2 w-32 h-32 items-center justify-center">
             <FaPython className="w-10 h-10"/>
             <h3>Python</h3>
           </CardContent>
         </Card>
-        <Card className="bg-[#706565] cursor-pointer hover:scale-150 transition duration-300 ease-in-out">
+        <Card className="bg-[#706565] cursor-pointer hover:scale-150 transition duration-300 ease-in-out  hover:text-white hover:bg-sky-400 hover:font-semibold">
           <CardContent className="flex flex-col gap-2 p-2 w-32 h-32 items-center justify-center">
             <FaPython className="w-10 h-10"/>
             <h3>Python</h3>
